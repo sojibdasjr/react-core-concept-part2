2e5adc73a4927d631f5e4092eb51e6c55229c463
sojibdasjr <contact.sojibdas@gmail.com>
2024-02-13 19:04:09 +0600

    Events Handlers Try in App jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import "./App.css";
 
 function App() {
+  // Evend Handeler Normal Function way 1
+  function handleClick (){
+    alert('Button Click')
+  }
+
+  // Event Handelar Way 2 
+  const handleClick2 = () =>{
+    alert("Button Click 2")
+  }
+
+  const addToFive = (num)=>{
+    alert(num + 5 )
+  }
   return (
     <>
       <h3>React Core Concept 2</h3>
+      <button onClick={handleClick}>Click Button 1</button>
+      <button onClick={handleClick2}>Click Button 2</button>
+      <button onClick={()=>{alert('button Click 3')}}>Click Button 3</button>
+      <button onClick={()=> addToFive(3)}>Four</button>
     </>
   );
 }
